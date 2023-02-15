0076dda87aadab403929db01e6bdcb4a01068acf
Kiratobu <bronislav1996@gmail.com>
2023-02-15 16:46:16 +0600

    Adding admin settings
    Добавление поиска и фильтрации в панель администратора

--- app/agriculture/admin.py
+++ app/agriculture/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from agriculture.models import (
+    Farmer,
+    Culture,
+    Plot,
+    Season
+)
+
+@admin.register(Farmer)
+class FarmerAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    search_fields = ('name',)
+    list_filter = ('name',)
+ 
+@admin.register(Culture)
+class CultureAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    search_fields = ('name',)
+    list_filter = ('name',)
+
+admin.site.register(Plot)
+admin.site.register(Season)
