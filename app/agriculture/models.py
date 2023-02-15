8a5925b2c4b46de1213bf87671afb7168f2e2797
Kiratobu <bronislav1996@gmail.com>
2023-02-15 13:07:17 +0600

    Adding models
    Добавление моделей: фермеров, полей, культур, сезонов

--- app/agriculture/models.py
+++ app/agriculture/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
 # Create your models here.
+
+class Farmer(models.Model):
+
+    name = models.CharField(max_length=255, verbose_name='Имя')
+
+    def __str__(self):
+        return self.name
+    
+class Plot(models.Model):
+
+    name = models.CharField(max_length=255, verbose_name='Название поля')
+
+    def __str__(self):
+        return self.name
+    
+class Culture(models.Model):
+    
+    name = models.CharField(max_length=255,verbose_name='Название культуры')
+    
+    def __str__(self):
+        return self.name
+    
+class Season(models.Model):
+    
+    name = models.CharField(max_length=255,verbose_name='Название сезона')
+    
+    def __str__(self):
+        return self.name
